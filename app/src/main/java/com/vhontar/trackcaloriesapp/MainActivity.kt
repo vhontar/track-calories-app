f6c26602c91a5b1478f8752462c989fd49a86f12
Vladyslav Hontar <vladislav.hontar@gmail.com>
2022-12-24 15:59:00 +0100

    Add compose themes/colors/types

--- app/src/main/java/com/vhontar/trackcaloriesapp/MainActivity.kt
+++ app/src/main/java/com/vhontar/trackcaloriesapp/MainActivity.kt
@@ -3,41 +3,15 @@ package com.vhontar.trackcaloriesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.vhontar.trackcaloriesapp.ui.theme.TrackCaloriesAppTheme
+import com.vhontar.trackcaloriesapp.ui.theme.CaloryTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TrackCaloriesAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            CaloryTrackerTheme {
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TrackCaloriesAppTheme {
-        Greeting("Android")
     }
 }
