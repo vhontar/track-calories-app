21caba6cc0847070dac33a2a52f312a308cddcab
Vladyslav Hontar <vladislav.hontar@gmail.com>
2022-12-26 14:00:53 +0100

    Setup remote/local data providers for tracking food

---
+++ tracker/tracker_data/src/main/java/com/vhontar/tracker_data/di/TrackerDataModule.kt
@@ -0,0 +1,47 @@
+package com.vhontar.tracker_data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.vhontar.tracker_data.local.TrackerDatabase
+import com.vhontar.tracker_data.remote.OpenFoodApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+    @Provides
+    @Singleton
+    fun providesTrackerDatabase(@ApplicationContext context: Context): TrackerDatabase =
+        Room.databaseBuilder(
+            context,
+            TrackerDatabase::class.java,
+            "tracker_db"
+        ).build()
+
+    @Provides
+    @Singleton
+    fun providesOpenFoodApi(client: OkHttpClient): OpenFoodApi = Retrofit.Builder()
+        .baseUrl(OpenFoodApi.BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .client(client)
+        .build()
+        .create(OpenFoodApi::class.java)
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+}
---
+++ tracker/tracker_data/src/main/java/com/vhontar/tracker_data/remote/dto/SearchDto.kt
@@ -0,0 +1,5 @@
+package com.vhontar.tracker_data.remote.dto
+
+data class SearchDto(
+    val products: List<Product>,
+)
